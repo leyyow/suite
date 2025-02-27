b30736bdd36fc361ab5b909a84fc0cc46f862ba0
Theo Ojochegbe <symplytheo@gmail.com>
2025-02-27 09:01:43 +0100

    chore: enable auto position & prevent multiple opened dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
+<script setup>
+import { provide, ref } from "vue";
+
+// ============ Prevent Multiple Dropdown ===== //
+// ============================================//
+const dropdowns = ref([]);
+// Register a dropdown to allow centralized control
+const registerDropdown = (closeFn) => {
+  dropdowns.value.push(closeFn);
+};
+// Close all open dropdowns
+const closeAllDropdowns = () => {
+  dropdowns.value.forEach((closeFn) => closeFn());
+};
+// Provide these functions to all child components
+provide("registerDropdown", registerDropdown);
+provide("closeAllDropdowns", closeAllDropdowns);
+</script>
+
 <template>
   <RouterView />
 </template>
