652bce3fd09a09f67f091c907d41140492b6f94f
Theo Ojochegbe <symplytheo@gmail.com>
2025-03-12 20:00:33 +0100

    feat: create api query-mutation list for sales inventory/orders

---
+++ src/composables/api/sales/inventory.js
@@ -0,0 +1,56 @@
+import { useApiMutation, useApiQuery } from "./api";
+
+export function useGetInventory(params = {}) {
+  return useApiQuery("/inventory/product/", params);
+}
+
+export function useBulkUploadProducts() {
+  return useApiMutation((data) => ({
+    url: "/inventory/product/create/bulk/",
+    method: "POST",
+    body: data,
+    headers: { "Content-Type": "multipart/form-data" },
+  }));
+}
+
+export function useCreateProduct() {
+  return useApiMutation((data) => ({
+    url: "/inventory/product/create/",
+    method: "POST",
+    body: data,
+    headers: { "Content-Type": "multipart/form-data" },
+  }));
+}
+
+export function useUpdateProduct() {
+  return useApiMutation(({ id, payload }) => ({
+    url: `/inventory/product/update/${id}/`,
+    method: "PATCH",
+    body: payload,
+    headers: { "Content-Type": "application/json" },
+  }));
+}
+
+export function useDeleteProduct() {
+  return useApiMutation((id) => ({
+    url: `/inventory/product/delete/${id}/`,
+    method: "PATCH",
+  }));
+}
+
+export function useUploadProductImages() {
+  return useApiMutation((data) => ({
+    url: "/inventory/product/image_create/",
+    method: "POST",
+    body: data,
+    headers: { "Content-Type": "multipart/form-data" },
+  }));
+}
+
+export function useUpdateProductImage() {
+  return useApiMutation((data) => ({
+    url: "/inventory/product/image_product_update/",
+    method: "PUT",
+    body: data,
+  }));
+}
--- src/composables/api/sales/orders.js
+++ src/composables/api/sales/orders.js
@@ -1,9 +1,50 @@
-import { useApiQuery } from "./api";
+import { useApiMutation, useApiQuery } from "./api";
 
-export function useGetOrdersApi(params) {
-  return useApiQuery(`/inventory/order/`, params);
+export function useGetOrders(params = {}) {
+  return useApiQuery("/inventory/order/", params);
 }
 
-export function useGetOrdersSummaryApi(params) {
-  return useApiQuery(`/inventory/order/metrics/`, params);
+export function useGetOrderItems(id) {
+  return useApiQuery(`/inventory/order_items/${id}/`);
+}
+
+export function useCreateOrder() {
+  return useApiMutation((data) => ({
+    url: "/inventory/order/create/",
+    method: "POST",
+    body: data,
+    headers: { "Content-Type": "application/json" },
+  }));
+}
+
+export function useUpdateOrder() {
+  return useApiMutation(({ id, payload }) => ({
+    url: `/inventory/order/update/${id}/`,
+    method: "PATCH",
+    body: payload,
+    headers: { "Content-Type": "application/json" },
+  }));
+}
+
+export function useDeleteOrder() {
+  return useApiMutation((id) => ({
+    url: `/inventory/order/delete/${id}/`,
+    method: "DELETE",
+  }));
+}
+
+export function useUpdateOrderItem() {
+  return useApiMutation(({ id, payload }) => ({
+    url: `/inventory/order_item/update/${id}/`,
+    method: "PATCH",
+    body: payload,
+    headers: { "Content-Type": "application/json" },
+  }));
+}
+
+export function useUpdateFulfilled() {
+  return useApiMutation((id) => ({
+    url: `/inventory/order_item/update_fulfilled/${id}/`,
+    method: "PATCH",
+  }));
 }
